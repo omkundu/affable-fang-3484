3c9a78a3a9231585b9a6c38117c8a8d520c0e014
omkundu <omkundu156@gmail.com>
2022-11-09 22:09:02 +0530

    day-2-task-done

--- mytheresa/src/AllRoutes/MainRoutes.jsx
+++ mytheresa/src/AllRoutes/MainRoutes.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+// import React from 'react'
+// import { Route, Routes } from 'react-router-dom'
+// import { Login } from '../Login/Login'
+// import { LoginAndRegister } from '../Login/LoginAndRegister'
+// import { Register } from '../Login/Register'
 
+import { Route, Routes } from "react-router-dom"
+import { LoginAndRegister } from "../Login/LoginAndRegister"
+import HomePage from "./HomePage"
 const MainRoutes = () => {
   return (
-    <div>MainRoutes</div>
+    <Routes>
+            <Route path='/' element={<HomePage/>}></Route>
+            <Route path='/Register' element={<LoginAndRegister/>}></Route>
+
+    </Routes>
   )
 }
 
