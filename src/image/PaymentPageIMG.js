556800cd81a07b88da70a132e7d81cf2ee8a4736
theshenron <Gourav143faz@gmail.com>
2022-11-10 19:28:49 +0530

    Payment Gatway ...

--- src/image/PaymentPageIMG.js
+++ src/image/PaymentPageIMG.js
@@ -1,1 +1,3 @@
 export const HeaderIMG = "https://www.mytheresa.com/skin/frontend/mytheresa/default/images/logo.png?v=20221102T163423"
+export const BoxIMG1 = "https://www.mytheresa.com/skin/frontend/mytheresa/default/images/package/package_mytheresa_2x.jpg"
+export const BoxIMG2 = "https://www.mytheresa.com/skin/frontend/mytheresa/default/images/package/package_brown_2x.jpg"
