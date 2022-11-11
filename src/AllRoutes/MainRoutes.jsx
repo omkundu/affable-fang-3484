009c62b7926f85214ac96823f51503e8ba3c818b
omkundu <omkundu156@gmail.com>
2022-11-11 22:24:30 +0530

    day-4-work-done

--- src/AllRoutes/MainRoutes.jsx
+++ src/AllRoutes/MainRoutes.jsx
@@ -4,7 +4,7 @@ import { Routes, Route } from "react-router-dom"
 // import { Login } from '../Components/Login/Login';
 import  {LoginAndRegister} from "../Components/Login/LoginAndRegister"
 
-import { Login } from '../Components/Login/Login';
+// import { Login } from '../Components/Login/Login';
 
 import { WomenPage } from '../Pages/women/WomenPage';
 import Delivery from "../Pages/Delivery"
@@ -18,9 +18,10 @@ function MainRoutes() {
       <Route path='/' element={
           <WomenPage />
       } />
-
-      <Route path='/login' element={<Login/>} />
+      
+      <Route path='/login' element={<LoginAndRegister/>} />
       <Route path='/delivery' element={<Delivery/>} />
+     
 
 
     </Routes>
--- src/Components/Login/LoginAndRegister.jsx
+++ src/Components/Login/LoginAndRegister.jsx
@@ -1,16 +1,21 @@
+import { BottomNav } from "../Navbar/BottomNav"
+import { Dropdown } from "../Navbar/Dropdown"
+import { Navbar } from "../Navbar/Navbar"
 import { Login } from "./Login"
 import "./Login.css"
 import { Register } from "./Register"
 
 export const LoginAndRegister = () => {
     return (
+        <>
+         <div><Navbar/></div>
+         {/* <div><Dropdown/></div> */}
+        
         <div className="Main">
-           
             <div><Register /></div>
-            
             <div><Login /></div>
-           
-            
         </div>
+
+        </>
     )
 }
