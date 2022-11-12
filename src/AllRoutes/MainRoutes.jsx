2fc60c67eee77c8ae05ef398b632ea695c3e54f3
theshenron <gourav143faz@gmail.com>
2022-11-12 11:48:12 +0530

    files changed

--- src/AllRoutes/MainRoutes.jsx
+++ src/AllRoutes/MainRoutes.jsx
@@ -1,44 +1,27 @@
 import React from 'react';
 import { Routes, Route } from "react-router-dom"
-// import { Footer } from "../components/Footer/Footer"
-// import { Login } from '../Components/Login/Login';
-import  {LoginAndRegister} from "../Components/Login/LoginAndRegister"
-// import {MenPage} from "../Pages/MenPage/MenPage"
 
-
-
-// import { Login } from '../Components/Login/Login';
-
-import {MenPage} from "../Pages/MenPage/MenPage"
-import { Login } from '../Components/Login/Login';
-
-
+import { LoginAndRegister } from "../Components/Login/LoginAndRegister"
+import { MenPage } from "../Pages/MenPage/MenPage"
 import { WomenPage } from '../Pages/women/WomenPage';
 import Delivery from "../Pages/Delivery"
+import WomenProducts from "../Pages/WomenProducts"
+import { ProductDetailsPage } from "../Pages/ProductDetailsPage"
 
 import PrivateRoute from './PrivateRoutes/PrivateRoute';
-
 
 
 function MainRoutes() {
   return (
     <Routes>
 
-      <Route path='/' element={
-          <WomenPage />
-      } />
-      {/* <Route path="/men" element={<MenPage/>}> </Route> */}
-      <Route path='/login' element={<LoginAndRegister/>} />
-      <Route path='/delivery' element={<Delivery/>} />
-      
-
-      <Route path='/' element={<WomenPage /> } />
-      <Route path="/men" element={<MenPage/>}> </Route>
-
-      <Route path='/login' element={<LoginAndRegister/>} />
-      <Route path='/delivery' element={<Delivery/>} />
-     
-
+      <Route path='/' element={<WomenPage />} />
+      <Route path='/' element={<WomenPage />} />
+      <Route path="/men" element={<MenPage />}> </Route>
+      <Route path='/login' element={<LoginAndRegister />} />
+      <Route path='/delivery' element={<Delivery />} />
+      <Route path='/womenproducts' element={<WomenProducts />} />
+      <Route path='/WomenProducts/:id' element={<ProductDetailsPage />} />
 
     </Routes>
   );
