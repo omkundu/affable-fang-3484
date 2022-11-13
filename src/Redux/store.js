da6e0b0c1ba3bb4df008a6fb171f6580ed717d14
theshenron <gourav143faz@gmail.com>
2022-11-13 18:28:24 +0530

    V

--- src/Redux/store.js
+++
@@ -1,9 +0,0 @@
-// import { reducer as AppRedux } from "../Redux/AppRedux/reducer";
-import { reducer as AuthRedux } from "../Redux/AuthRedux/reducer";
-import { reducer as WomenReducer } from "./WomenReducer/reducer";
-
-import thunk from "redux-thunk";
-import { combineReducers, applyMiddleware, legacy_createStore } from "redux";
-
-const rootReducer = combineReducers({ WomenReducer,AuthRedux });
-export const store = legacy_createStore(rootReducer, applyMiddleware(thunk));
